4b70f50eeb6b48632cfe98ec22629aa8247e41c0
mathiasmantai <mmantaibusiness@gmail.com>
2024-01-16 12:21:23 +0100

    feat: open file and display content in multi line entry

---
+++ README.md
@@ -0,0 +1,4 @@
+### Features
+- input fields before and after every field
+- checkbox, comma separated list
+- checkbox, comma after last element
--- main.go
+++ main.go
@@ -1,20 +1,63 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	root := app.New()
-	window := root.NewWindow("Root")
+	myApp := app.New()
 
-	queryTemplate := widget.NewEntry()
-	queryTemplate.SetPlaceHolder("Enter Query Template")
+	myWindow := myApp.NewWindow("File Dialog Example")
+	size := fyne.NewSize(800, 600)
+	myWindow.Resize(size)
 
-	content := container.NewVBox(queryTemplate)
+	entry := widget.NewEntry()
+	entry.MultiLine = true
 
-	window.SetContent(content)
-	window.ShowAndRun()
+	openFileDialogButton := widget.NewButton("Open File Dialog", func() {
+		openFileDialog(myWindow, entry)
+	})
+
+
+
+	content := container.NewVBox(
+		openFileDialogButton,
+		entry,
+	)
+
+	myWindow.SetContent(content)
+	myWindow.ShowAndRun()
+}
+
+func openFileDialog(window fyne.Window, entry *widget.Entry) {
+	fileDialog := dialog.NewFileOpen(func(file fyne.URIReadCloser, err error) {
+		if err != nil {
+			// Handle error
+			return
+		}
+
+		if file == nil {
+			// Dialog was canceled
+			return
+		}
+		defer file.Close()
+
+		data := make([]byte, 1024)
+		n, err := file.Read(data)
+		if err != nil {
+            // Handle error
+            return
+        }
+
+		entry.SetText(string(data[:n]))
+	}, window)
+
+	fileDialog.SetFilter(storage.NewExtensionFileFilter([]string{".txt", ".csv"})) // You can set filters if needed
+
+	fileDialog.Show()
 }
